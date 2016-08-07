ee0276e9de0204f076d65de90516d9e13191098b
quinn0 <quinn.mcfee@gmail.com>
2016-08-07 15:04:09 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix was made using the example as a template 
+#as you can see the outer most function deals with the lexical
+#scoping we need to access/re-assign inv which becomes the 
+#output of the function
 
-## Write a short comment describing this function
-
+## Using the same set/get style as example:
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL  #setting initial 'unchached' matrix
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <-solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Here if the matrix (not actually a matrix yet in null state) is not NULL then 
+## we know there is an inverse cached  otherwise we set the value of the solved
+##matrix from makeCacheMatrix and return inv
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if (!is.null(inv)){ 
+                print("Getting cached data")
+                return(inv)
+        }else
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
+##Stay hydrated and enjoy your day!
